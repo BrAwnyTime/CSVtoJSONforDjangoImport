791ff5f73d91c1df5b5dcc6cd9b5f09157404a2c
unknown <baune@BAUNE-THINK.internal.crg>
2015-08-03 13:01:44 -0400

    Initial commit

---
+++ convert csv to json.py
@@ -0,0 +1,27 @@
+import csv
+import json
+
+csvfile = open('import.csv', 'r')
+jsonfile = open('import.json', 'w')
+model = "DjanogApp.DjangoModel"
+pkstart = 0
+
+jsonfile.write("[")
+reader = csv.DictReader( csvfile)
+i = 0
+for row in reader:
+  if i != 0:
+    jsonfile.write(',\n')
+  d = {}
+  d["fields"] = row
+  d["model"] = model
+  d["pk"] = i + pkstart
+  i += 1
+  json.dump(d, jsonfile)
+
+jsonfile.write("]")
+'''
+surround the whole file with []
+add '{"fields":' to the start of each line
+add '},"model": "chemdemo.chemical","pk": ###},' to the end of each line where ### is a unique number
+'''
